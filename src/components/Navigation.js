48255966f9f331e7092480fc28ef15617b53414e
mdimo <mihaylo.dimo@gmail.com>
2022-07-24 20:37:35 +0300

    Add texture as background

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,17 @@
 import * as React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import "../styles/Navigation.css"
 
 
 export const Navigation = () => {
     return (
         <nav>
-            <Link to="/"> Home </Link>
-            <Link to="/anime-list"> Anime-list </Link>
-            <Link to="/create-profile"> Create Profile </Link>
-            <Link to="/profile"> Profile </Link>
-            <Link to="/login"> Login </Link>
-            <Link to="/logout"> Logout </Link>
+            <Link to="/">Home</Link>
+            <Link to="/anime-list">Anime-list</Link>
+            <Link to="/create-profile">Create Profile</Link>
+            <Link to="/profile">Profile</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/logout">Logout</Link>
         </nav>
     ); 
 }
